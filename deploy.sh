4ffd9ffea4cf638c89c2f38f2d00ececf5959cee
Andrew Brookins <andrew.brookins@redislabs.com>
2020-09-23 19:22:53 -0700

    WIP on deploy scripts; fix unicode error

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ docker build -t $TAG --build-arg REDIS_PASSWORD=$REDIS_PASSWORD .
 
 echo "Updating compute engine container"
 docker push $TAG
-gcloud compute instances update-container docsearch-app-west --container-image $TAG
+#gcloud compute instances update-container docsearch-app-west --container-image $TAG
--- docsearch/connections.py
+++ docsearch/connections.py
@@ -10,9 +10,12 @@ INDEX = "docs"
 load_dotenv()
 
 
-def get_search_connection(password=os.environ['REDIS_PASSWORD'], host=os.environ['REDIS_HOST']):
-    return Client(INDEX, password=password, host=host)
-
-
 def get_redis_connection(password=os.environ['REDIS_PASSWORD'], host=os.environ['REDIS_HOST']):
     return Redis(password=password, host=host, decode_responses=True)
+
+
+def get_search_connection(password=os.environ['REDIS_PASSWORD'], host=os.environ['REDIS_HOST']):
+    conn = get_redis_connection()
+    return Client(INDEX, conn=conn, password=password, host=host)
+
+
