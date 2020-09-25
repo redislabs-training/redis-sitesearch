ebc7dafb54722e547eb23dc1b8282934bc7696b2
Andrew Brookins <andrew.brookins@redislabs.com>
2020-09-24 17:45:40 -0700

    fix CORS for local testing

--- docsearch/api/__init__.py
+++ docsearch/api/__init__.py
@@ -7,8 +7,9 @@ from .indexer import IndexerResource
 cors = CORS(allow_origins_list=[
     'http://docs.andrewbrookins.com:1313',
     'https://docs.redislabs.com',
-    'http://localhost:8080']
-)
+    'http://localhost:8080',
+    'http://localhost:1313'
+])
 
 api = falcon.API(middleware=[cors.middleware])
 api.add_route('/search', SearchResource())
