849d3e6f6fcd336aa9265ffe3b5005517c64bf8c
Andrew Brookins <andrew.brookins@redislabs.com>
2020-09-25 17:06:42 -0700

    Tweaks to mounting a data disk

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ NEW_TEMPLATE="docsearch-app-west-$HASH"
 SERVICE_ACCOUNT="279443788353-compute@developer.gserviceaccount.com"
 US_WEST_DISK="docsearch-app-west-2"
 
-#echo "Building $TAG..."
-#docker build -t $TAG --build-arg REDIS_PASSWORD=$REDIS_PASSWORD .
+echo "Building $TAG..."
+docker build -t $TAG --build-arg REDIS_PASSWORD=$REDIS_PASSWORD .
 
-#echo "Updating compute engine container"
-#docker push $TAG
+echo "Updating compute engine container"
+docker push $TAG
 
 echo "Creating new us-west instance template $NEW_TEMPLATE from $TAG"
 gcloud beta compute --project=redislabs-university instance-templates \
