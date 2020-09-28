66e87e9fb70c16cf794dfb2066891e232ab0cc98
Andrew Brookins <andrew.brookins@redislabs.com>
2020-09-27 19:17:43 -0700

    Use redisearch 2.0 index definition

--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -77,54 +77,51 @@ def test_indexer_indexes_page_document(index_file):
         'score': 0.6989700043360187
     }
     key = keys.document('http://example.com', expected_doc['doc_id'])
-    print(indexer.search_client.redis.hset.call_args_list)
     indexer.search_client.redis.hset.assert_any_call(key, mapping=expected_doc)
 
 
 def test_indexer_indexes_page_section_documents(index_file):
     indexer = index_file(FILE_WITH_SECTIONS)
-
+    page_url = f"{SITE}/test"
     expected_section_docs = [
         {
-            'doc_id': f'{SITE}/test:Database Persistence with Redis Enterprise Software:Options for configuring data persistence:0',
+            'doc_id': f'{page_url}:Database Persistence with Redis Enterprise Software:Options for configuring data persistence:0',
             'title': 'Database Persistence with Redis Enterprise Software',
             'section_title': 'Options for configuring data persistence',
             'hierarchy': '["Redis Enterprise Software", "Concepts and Architecture", "Data Access Architecture", "Database Persistence with Redis Enterprise Software"]',
-            'url': 'example.com/test',
+            'url': page_url,
             'body': 'There are six\xa0options for persistence in Redis Enterprise Software:    Options Description     None Data is not persisted to disk at all.   Append Only File (AoF) on every write Data is fsynced to disk with every write.   Append Only File (AoF) one second Data is fsynced to disk every second.   Snapshot every 1 hour A snapshot of the database is created every hour.   Snapshot every 6 hours A snapshot of the database is created every 6 hours.   Snapshot every 12 hours A snapshot of the database is created every 12 hours.    The first thing you need to do is determine if you even need persistence. Persistence is used to recover from a catastrophic failure, so make sure that you need to incur the overhead of persistence before you select it. If the database is being used as a cache, then you may not need persistence. If you do need persistence, then you need to identify\xa0which is the best type for your use case.',
             'type': 'section',
             'position': 0,
             'score': 0.1989700043360188,
-            'replace': True
         },
         {
-            'doc_id': f'{SITE}/test:Database Persistence with Redis Enterprise Software:Append only file (AOF) vs snapshot (RDB):1',
+            'doc_id': f'{page_url}:Database Persistence with Redis Enterprise Software:Append only file (AOF) vs snapshot (RDB):1',
             'title': 'Database Persistence with Redis Enterprise Software',
             'section_title': 'Append only file (AOF) vs snapshot (RDB)',
             'hierarchy': '["Redis Enterprise Software", "Concepts and Architecture", "Data Access Architecture", "Database Persistence with Redis Enterprise Software"]',
-            'url': 'example.com/test',
+            'url': page_url,
             'body': 'Now that you know the available options, to assist in making a decision on which option is right for your use case, here is a table about the two:    Append Only File (AOF) Snapshot (RDB)     More resource intensive Less resource\xa0intensive   Provides better durability (recover the latest point in time) Less durable   Slower time to recover (Larger files) Faster recovery time   More disk space required (files tend to grow large and require compaction) Requires less resource (I/O once every several hours and no compaction required)',
             'type': 'section',
             'position': 1,
             'score': 0.1989700043360188,
-            'replace': True
         },
         {
             'doc_id': f'{SITE}/test:Database Persistence with Redis Enterprise Software:Data persistence and Redis on Flash:2',
             'title': 'Database Persistence with Redis Enterprise Software',
             'section_title': 'Data persistence and Redis on Flash',
             'hierarchy': '["Redis Enterprise Software", "Concepts and Architecture", "Data Access Architecture", "Database Persistence with Redis Enterprise Software"]',
-            'url': 'example.com/test',
+            'url': page_url,
             'body': 'If you are enabling data persistence for databases running on Redis Enterprise Flash, by default both master and slave shards are configured to write to disk. This is unlike a standard Redis Enterprise Software database where only the slave shards persist to disk. This master and slave dual data persistence with replication is done to better protect the database against node failures. Flash-based databases are expected to hold larger datasets and repair times for shards can be longer under node failures. Having dual-persistence provides better protection against failures under these longer repair times. However, the dual data persistence with replication adds some processor and network overhead, especially in the case of cloud configurations with persistent storage that is network attached (e.g. EBS-backed volumes in AWS). There may be times where performance is critical for your use case and you don’t want to risk data persistence adding latency. If that is the case, you can disable data-persistence on the master shards using the following\xa0rladmin command: rladmin tune db db: master_persistence disabled',
             'type': 'section',
             'position': 2,
-            'score': 0.1989700043360188,
-            'replace': True
+            'score': 0.1989700043360188
         }
     ]
 
     for doc in expected_section_docs:
-        indexer.search_client.add_document.assert_any_call(**doc)
+        key = keys.document('http://example.com', doc['doc_id'])
+        indexer.search_client.redis.hset.assert_any_call(key, mapping=doc)
 
 
 def test_document_parser_skips_pages_without_breadcrumbs(parse_file):
