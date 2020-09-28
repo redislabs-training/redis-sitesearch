cda051acee043c7e1c4b36fdfeeaae6eee424149
Andrew Brookins <andrew.brookins@redislabs.com>
2020-09-28 14:06:17 -0700

    More attempts to fix health check

--- docsearch/api/health.py
+++ docsearch/api/health.py
@@ -16,18 +16,37 @@ log = logging.getLogger(__name__)
 queue = Queue(connection=redis_client)
 registry = StartedJobRegistry('default', connection=redis_client)
 
+IN_PROGRESS_JOB_STATES = (
+    JobStatus.QUEUED,
+    JobStatus.STARTED,
+    JobStatus.FINISHED,
+    JobStatus.FAILED
+)
+
 
 class HealthCheckResource:
     def on_get(self, req, resp):
         """Make sure the search index is available."""
+        reindex = False
+
         try:
             info = search_client.info()
         except ResponseError as e:
-            log.error("Health check failed: %s", e)
-            queue.enqueue(index, job_id=JOB_ID, job_timeout=INDEXING_TIMEOUT)
-            resp.status = falcon.HTTP_503
-            return
+            log.error("Response error: %s", e)
+            reindex = True
+        else:
+            if info['num_docs'] == "0":
+                reindex = True
 
-        if info['num_docs'] == "0":
-            queue.enqueue(index, job_id=JOB_ID, job_timeout=INDEXING_TIMEOUT)
-            resp.status = falcon.HTTP_503
+        if reindex:
+            try:
+                status = Job.fetch(JOB_ID, connection=redis_client).get_status()
+            except NoSuchJobError:
+                if JOB_ID in registry.get_job_ids():
+                    # Indexing has already started
+                    return
+                status = None
+        
+            if status not in IN_PROGRESS_JOB_STATES:
+                queue.enqueue(index, job_id=JOB_ID, job_timeout=INDEXING_TIMEOUT)
+                resp.status = falcon.HTTP_503
