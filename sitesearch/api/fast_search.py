54265eb40a3a0bbca9a79b9c8a3ba1e271dd7be6
Andrew Brookins <andrew.brookins@redislabs.com>
2021-03-09 14:54:10 -0800

    wip on fastapi

--- sitesearch/api/fast_search.py
+++ sitesearch/api/fast_search.py
@@ -1,65 +1,55 @@
 import json
 import logging
+from typing import Optional
 
 import redis
-from fastpi import FasAPI,header
+from fastapi import FastAPI, Query, HTTPException
 
 from sitesearch.config import Config
 from sitesearch.transformer import transform_documents
 from sitesearch.connections import get_search_connection, get_redis_connection
 from sitesearch.query_parser import parse
 from sitesearch import indexer
-from .resource import Resource
 
 config = Config()
 redis_client = get_redis_connection()
 log = logging.getLogger(__name__)
+app = FastAPI()
 
 DEFAULT_NUM = 30
 MAX_NUM = 100
 
 
-class SearchResource(Resource):
-    def on_get(self, req, resp):
-        """Run a search."""
-        query = req.get_param('q', default='')
-        from_url = req.get_param('from_url', default='')
-        start = int(req.get_param('start', default=0))
-        site_url = req.get_param('site', default=config.default_search_site.url)
-        search_site = config.sites.get(site_url, config.default_search_site)
-        section = indexer.get_section(site_url, from_url)
+async def search(q: Optional[str] = Query(""),
+                 from_url: Optional[str] = Query(""),
+                 start: Optional[int] = Query(0),
+                 num: Optional[int] = Query(DEFAULT_NUM),
+                 site_url: Optional[str] = Query(config.default_search_site.url),
+                 search_site: Optional[str] = Query(config.default_search_site)):
+    search_site = config.sites.get(site_url, config.default_search_site)
+    section = indexer.get_section(site_url, from_url)
 
-        if not search_site:
-            raise falcon.HTTPBadRequest(
-                "Invalid site", "You must specify a valid search site.")
+    if not search_site:
+        raise HTTPException(
+            status_code=400, detail="You must specify a valid search site.")
 
-        try:
-            num = min(int(req.get_param('num', default=DEFAULT_NUM)), MAX_NUM)
-        except ValueError:
-            num = DEFAULT_NUM
+    num = min(num, MAX_NUM)
+    search_client = get_search_connection(search_site.index_alias)
+    query = parse(q, section, search_site).paging(start, num)
 
-        search_client = get_search_connection(search_site.index_alias)
+    try:
+        res = search_client.search(query)
+    except (redis.exceptions.ResponseError, UnicodeDecodeError) as e:
+        log.error("Search query failed: %s", e)
+        total = 0
+        docs = []
+    else:
+        docs = res.docs
+        total = res.total
 
-        try:
-            num = min(int(req.get_param('num', default=DEFAULT_NUM)), MAX_NUM)
-        except ValueError:
-            num = DEFAULT_NUM
+    docs = transform_documents(docs, search_site, q.query_string())
 
-        q = parse(query, section, search_site).paging(start, num)
-
-        try:
-            res = search_client.search(q)
-        except (redis.exceptions.ResponseError, UnicodeDecodeError) as e:
-            log.error("Search query failed: %s", e)
-            total = 0
-            docs = []
-        else:
-            docs = res.docs
-            total = res.total
-
-        docs = transform_documents(docs, search_site, q.query_string())
-
-        resp.body = json.dumps({
-            "total": total,
-            "results": docs
-        })
+    return json.dumps({
+        "total": total,
+        "results": docs
+    })
