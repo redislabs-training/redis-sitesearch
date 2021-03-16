1ef84f40ad9983f8727c9d788497c839b20decb3
Andrew Brookins <andrew.brookins@redislabs.com>
2021-03-16 00:52:36 -0700

    Give worker nodes a health check endpoint

--- sitesearch/api/health.py
+++ sitesearch/api/health.py
@@ -1,8 +1,10 @@
 import logging
-from sitesearch import keys
 
+import newrelic
 from falcon.status_codes import HTTP_503
 from redis.exceptions import ResponseError
+
+from sitesearch import keys
 from sitesearch.config import Config
 from sitesearch.connections import get_search_connection, get_rq_redis_client
 from .resource import Resource
@@ -17,6 +19,7 @@ class HealthCheckResource(Resource):
         """
         This service is considered unhealthy if the default search index is unavailable.
         """
+        newrelic.agent.ignore_transaction(flag=True)
         try:
             search_client.info()
         except ResponseError as e:
