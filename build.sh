e873e7c3d49241aa902dd5cafcc03eec6d23c57a
Andrew Brookins <andrew.brookins@redisl>
2021-09-29 10:51:05 -0700

    Try to serialize on the top-level job, at least

--- build.sh
+++
@@ -1,7 +0,0 @@
-
-HASH=`git rev-parse --short HEAD`
-TAG=sitesearch-app:$HASH
-IMAGE="gcr.io/redislabs-university/$TAG"
-
-echo "Building $IMAGE..."
-docker build -t $TAG --build-arg "REDIS_PASSWORD=$REDIS_PASSWORD" .
