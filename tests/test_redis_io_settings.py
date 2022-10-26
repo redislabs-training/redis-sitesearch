c4606034c6759dfe16d52f1116e714096dcba7f4
Kyle Banker <banker@users.noreply.github.com>
2022-10-26 00:08:25 -0600

    minor: fix test

--- tests/test_redis_io_settings.py
+++ tests/test_redis_io_settings.py
@@ -8,7 +8,7 @@ def test_get_landing_page():
    assert REDIS_IO_LANDING_PAGES['get'].body == 'Get the value of a key'
 
 def test_correct_number_of_landing_pages():
-   assert len(REDIS_IO_LANDING_PAGES.keys()) == 259
+   assert len(REDIS_IO_LANDING_PAGES.keys()) == 723
 
 def test_ignore_commands_with_spaces():
    assert 'acl' in REDIS_IO_LANDING_PAGES
